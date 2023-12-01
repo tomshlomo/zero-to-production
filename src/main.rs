2bf01fd166686a7a7dcf78964a22eec92f7c9233
tom.shlomo <tom.shlomo@ridewithvia.com>
2023-12-01 22:39:36 +0200

    first commit

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+use std::net::TcpListener;
+
+use zero2prod::run;
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    run(listener)?.await
+}
